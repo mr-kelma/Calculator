3fad09a8b3da589bde676e662566ed62b7e91724
Valery Keplin <98012564+mr-kelma@users.noreply.github.com>
2022-06-06 11:45:10 +0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Valery Keplin on 6.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
